b4f943c90ebac27890c8f51ad8634120ee7e1b27
Roland.Germ <roland.germ@nagarro.com>
2020-07-27 11:17:59 +0200

    playing aroiund with key actions

--- src/app/country-search/country.component.ts
+++ src/app/country-search/country.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 
 import {Country} from '../model/country';
 import {CountrySearchService} from '../services/country-search-service';
+import {interval, Observable, Subject} from "rxjs";
+import {map, startWith, switchMap, tap} from "rxjs/operators";
 
 @Component({
     selector: 'app-country',
@@ -11,9 +13,22 @@ import {CountrySearchService} from '../services/country-search-service';
 export class CountryComponent {
     loading = false;
     countries: Country [];
+    private keySubject = new  Subject<string>();
+    keyActions$ = this.keySubject.asObservable();
+    countries$ : Observable<Country>;
 
     constructor(private searchService: CountrySearchService) {
         this.countries = [];
+
+        this.keyActions$.pipe(
+            startWith(''),
+            tap(_ => console.log(`API CALL at ${new Date()}`))
+        ).subscribe(c => console.log(c));
+
+
+        this.countries$ = this.searchService.searchCountriesByName('').pipe(
+
+        );
     }
 
     doSearch(searchString: string) {
@@ -26,4 +41,15 @@ export class CountryComponent {
             });
     }
 
+    onKey(value: string) {
+        this.doSearch(value);
+
+        this.keySubject.next(value);
+
+        const result = this.keyActions$.pipe(switchMap((ev) => {
+            return interval(1000).pipe(map(f => ev + f));
+        }));
+        result.subscribe(x => console.log(x));
+
+    }
 }
