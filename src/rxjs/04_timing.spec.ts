8b2e937926c0d53442867a68b394a0aaf1004f15
rolger <roland.germ@gmail.com>
2021-05-06 12:42:13 +0200

    enhanced timing exercise

--- src/rxjs/04_timing.spec.ts
+++ src/rxjs/04_timing.spec.ts
@@ -1,12 +1,12 @@
 import {interval} from "rxjs";
 import {cold} from "jasmine-marbles";
-import {filter, take} from "rxjs/operators";
+import {delay, filter, take} from "rxjs/operators";
 import {of} from "rxjs/internal/observable/of";
 
 describe('Marble testing exercise with timing', () => {
 
     it('it should return the result asap', () => {
-        // TODO: what's going on? fix test code to change asynchronous result into synchronous
+        // TODO: what's going on? fix test code to change asynchronous result into synchronous - use TestSchulder
         const source = interval(20)
             .pipe(
                 take(10),
@@ -14,18 +14,28 @@ describe('Marble testing exercise with timing', () => {
             );
 
         // TODO: fix the expected timeframes
-        const expected = cold('abcde|', { a: 0, b: 2, c: 4, d: 6, e: 8 });
+        const expected = cold('abcde|', {a: 0, b: 2, c: 4, d: 6, e: 8});
 
         expect(source).toBeObservable(expected);
     });
 
-    it('should waits 20 frames before receive the value', () => {
+    it('should wait 20 frames before receive the value passing TestScheduler', () => {
         const source = of('a').pipe(
-            // TODO: implement a delay
-
+            // TODO: implement a delay with TestScheduler
         );
         const expected = cold('--(a|)');
 
         expect(source).toBeObservable(expected);
     });
+
+    it('should wait 20 frames before receive the value with TestSchulder run() method', () => {
+        const source = of('a').pipe(
+            // TODO: implement a delay
+            delay(20)
+        );
+        const expected = cold('--------------------(a|)');
+
+        expect(source).toBeObservable(expected);
+    });
+
 });
